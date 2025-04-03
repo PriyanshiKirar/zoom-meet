56c8a549e32bc6b3744148af7bd923980bca8a5e
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2025-04-03 18:58:26 +0530

    3 april

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -4,10 +4,22 @@ import { cn } from "@/lib/utils";
 import {
   CallControls,
   CallParticipantsList,
+  CallStatsButton,
   PaginatedGridLayout,
   SpeakerLayout,
 } from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LayoutList } from "lucide-react";
+
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 const MeetingRoom = () => {
   const [Layout, setLayout] = useState<CallLayoutType>("speaker-left");
@@ -40,6 +52,34 @@ const MeetingRoom = () => {
       </div>
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
         <CallControls />
+        <DropdownMenu>
+          <div className="flex items-center">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg[#4c535b]">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="border-[#1C1F2E] bg-[#1C1F2E]  text-white">
+            {["Grid", "Speaker-Left", "Speaker-Right"].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem
+                  onClick={() => {
+                    setLayout(item.toLocaleLowerCase() as CallLayoutType);
+                  }}
+                  className="cursor-pointer"
+                >
+                  {item}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-[#1C1F2E]" />
+              </div>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <CallStatsButton/>
+        <button onClick={()=>setshowParticipants((prev)=> !prev)}>
+<div className="curson-pointer rounded-2xl">
+
+</div>
+        </button>
       </div>
     </section>
   );
